be89f731acd18f21ed14fce3952fd34f7aab73fb
Gilang Mukharoom <gilangmukharom53@gmail.com>
2022-09-19 20:56:33 +0800

    update

---
+++ Titin/About Us/Titin/about-us.js
@@ -0,0 +1,49 @@
+// const cc=document.getElementById('aboutcc');
+// aboutcc.addEventListener('click', function(){
+//     const i = document.createElement('p');
+//     const b = document.createTextNode('Candidate College is an education platform organization that works to facilitate students in Indonesia at home and abroad to achieve a quality education system');
+//     i.appendChild(b);
+//     a.appendChild(i);
+
+// });
+
+// const cc=document.getElementById('aboutcc');
+// cc.addEventListener('click', function(){
+//     const p = document.querySelector('#aboutcc a p');
+//     p.innerHTML="Candidate College is an education platform organization that works to facilitate students in Indonesia at home and abroad to achieve a quality education system";
+// });
+
+// const cc=document.getElementById('aboutcc');
+// aboutcc.addEventListener('click', function(){
+//     alert('Candidate College is an education platform organization that works to facilitate students in Indonesia at home and abroad to achieve a quality education system');
+// });
+
+// const visi=document.getElementById('visi');
+// visi.addEventListener('click', function(){
+//     alert('Vision of Candidate College is to achieve quality equally for all Indonesian students at home and abroad')
+// });
+
+// const misi=document.getElementById('misi');
+// misi.addEventListener('click', function(){
+//     alert('Mission of Candidate College is to give contribute significanly to Indonesian students both at home and abroad in providing information and inspiration about education')
+// });
+
+function pesan(){
+    const tampil = document.getElementById("tampil");
+    tampil.style.backgroundColor='gray';
+    tampil.style.color='white';
+    tampil.style.width='300px';
+    tampil.innerHTML = "Candidate College is an education platform organization that works to facilitate students in Indonesia at home and abroad to achieve a quality education system'";
+}
+
+function visi(){
+    const visi = document.getElementById("visi");
+    visi.style.backgroundColor='gray';
+    visi.style.color='white';
+    visi.style.width='300px';
+    visi.innerHTML = 'Vision of Candidate College is to achieve quality equally for all Indonesian students at home and abroad';
+}
+
+
+
+
